fc7a9f5f2d84bc56f561e7310d3d80e7c28133d0
Sherwin Jupiter <jupiter@pivotmobileapp.com>
2016-12-26 12:15:28 -0400

    tyt

--- www/js/config.js
+++ www/js/config.js
@@ -1,8 +1,8 @@
 /*Driver App Configuration*/
 
 var krms_driver_config ={	
-	'ApiUrl':"",					
-	'DialogDefaultTitle':"",	
-	'PushProjectID':"",	
-	'APIHasKey':""
+	'ApiUrl':"http://mealoop.com/driver/api",					
+	'DialogDefaultTitle':"Mealoop Driver",	
+	'PushProjectID':"957384344808",	
+	'APIHasKey':"fed7b441b349bae8f146711fbd215e90"
 };
